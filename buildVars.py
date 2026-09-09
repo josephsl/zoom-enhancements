ae1f11e37ce6410ddb65b9f807ae2bb4c9450282
Joseph Lee <joseph.lee22590@gmail.com>
2026-09-08 22:32:10 -0600

    Zoom Enhancements 26.09.1
    Signed-off-by: Joseph Lee <joseph.lee22590@gmail.com>

--- buildVars.py
+++ buildVars.py
@@ -23,7 +23,7 @@ addon_info = AddonInfo(
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
 	addon_description=_("""An add-on which aims to enhance the experience while using Zoom and NVDA"""),
 	# version
-	addon_version="26.09",
+	addon_version="26.09.1",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
 	addon_changelog=_("""* Added compatibility with add-on settings registration mechanics in NVDA 2026.3."""),
